b4b9c91ffeca56fd5d99138aa3fb3720d8f02788
Sebastian Krigeris <sebastiankri123@gmail.com>
2022-10-18 12:41:57 -0300

    commit2

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 // Inicio
 
-alert("Bienveni@ a CineApp");
-alert("Por favor ingresa tu usuario y contraseña");
+alert("Bienvenido a Magic Cinema");
+alert("Por favor, en caso de no contar con un usuario y contraseña, crear una cuenta para poder visualizar y operar sobre nuestra tienda online.")
 
-let usuario = prompt("Para ingresar a nuestra web, por favor ingresa el nombre de usuario: cineapp").toLowerCase();
-
-while(usuario != "cineapp") {
-    usuario = prompt("Usario incorrecto, por favor, vuelve a ingresar los datos correspondientes.").toLowerCase();
-    
+function usuarioYContrasena(usuario, contrasena, email){
+    this.usuario = usuario;
+    this.contraseña = contrasena
+    this.email = email;
 }
 
-let contraseña = prompt("Para ingresar a nuestra web, por favor ingresa la siguiente contraseña: funcion123").toLowerCase();
+const usuario = prompt("Ingrese usuario a crear.");
+const contrasena = prompt("Ingrese contraseña a crear.");
+const email = prompt("Ingrese e-mail para recibir notificaciones de nuestra tienda online.");
 
-while(contraseña != "funcion123") {
-    contraseña = prompt("Contraseña incorrecta, por favor, vuelve a ingresar los datos correspondientes.").toLowerCase();
-}
+const cuenta = new usuarioYContrasena(usuario, contrasena, email);
 
-alert("Muy bien, has accedido con éxito a nuestra tienda virtual.");
 
-let ingresarNombre = prompt("Ingresa tu nombre para poder continuar").toLowerCase();
+alert("Ingresaste a nuestra web con éxito.")
 
-alert("Hola" + " " + ingresarNombre + ", a continuación verás un listado de todas nuestras funciones y una vez que lo decidas, procedé a optar por cual de ellas te gustaría recurrir.");
+let ingresarNombre = prompt("Ingresa tu nombre para poder continuar");
+
+alert("Hola" + " " + ingresarNombre + ", a continuación verás un listado de todas nuestras funciones y una vez que lo decidas, procedé a optar por cual de ellas te gustaría disfrutar en Magic Cinema.");
 alert("Funciones de la semana: Piratas del Caribe 5, Transformers 4, Matrix.");
 
 // Compra de entradas
 
-const peli1 = "piratas del caribe 5";
-const peli2 = "transformers 5";
-const peli3 = "matrix";
+const peliculas = {
+    peli1: "piratas del caribe 5",
+    peli2: "transformers 5",
+    peli3: "matrix"
+}
+
 let userChoice = prompt("Bienvenido a nuestro sitio oficial de ventas, por favor, elige una pelicula").toLowerCase();
 
 
-while((userChoice !== peli1) && (userChoice !== peli2) && (userChoice !== peli3)) {
+while((userChoice !== peliculas.peli1) && (userChoice !== peliculas.peli2) && (userChoice !== peliculas.peli3)) {
     alert("Película no valida")
     userChoice = prompt("Porfavor, seleccione una película que se encuentre en nuestra cartelera.").toLowerCase();
 }
@@ -39,19 +42,19 @@ while((userChoice !== peli1) && (userChoice !== peli2) && (userChoice !== peli3)
 switch(userChoice) {
     case "piratas del caribe 5":
 
-    alert("Gracias por elegir: Piratas del Caribe 5, ahora necesitamos que nos indiques en qué horario desearías ver esta película en CineApp");
+    alert("Gracias por elegir: Piratas del Caribe 5, ahora necesitamos que nos indiques en qué horario desearías ver esta película en Magic Cinema");
 
     break;
 
     case "transformers 4":
 
-    alert("Gracias por elegir: Transformers 4: El lado oscuro de la luna, ahora necesitamos que nos indiques en qué horario desearías ver esta película en CineApp");
+    alert("Gracias por elegir: Transformers 4: El lado oscuro de la luna, ahora necesitamos que nos indiques en qué horario desearías ver esta película en Magic Cinema");
 
     break;
 
     case "matrix":
 
-    alert("Gracias por elegir: Matrix: El quinto nivel, ahora necesitamos que nos indiques en qué horario desearías ver esta película en CineApp");
+    alert("Gracias por elegir: Matrix: El quinto nivel, ahora necesitamos que nos indiques en qué horario desearías ver esta película en Magic Cinema");
 
     break;
 
@@ -65,13 +68,15 @@ switch(userChoice) {
 
 alert("Horarios disponibles: 12:30hs, 15:30hs, 20:30hs")
 
-const hora1 = "12:30hs";
-const hora2 = "15:30hs";
-const hora3 = "20:30hs";
+const horarios = {
+    hora1: "12:30hs",
+    hora2: "15:30hs",
+    hora3: "20:30hs"
+}
 
 let horario = prompt("Por favor, elige el horario en el que deseas ver esta pelicula").toLowerCase();
 
-while((horario !== hora1) && (horario !== hora2) && (horario !== hora3)) {
+while((horario !== horarios.hora1) && (horario !== horarios.hora2) && (horario !== horarios.hora3)) {
     alert("Horario no válido");
     horario = prompt("Porfavor, seleccione un horario válido").toLowerCase();
 }
@@ -106,9 +111,51 @@ let ingresaEmail = prompt("Por favor, Ingresa tu e-mail.")
 
 alert("Gracias por adjuntarnos tu e-email, en las próximas 2hs te envíaremos el comprobante de la operación realizada, en conjunto de la ubicación en la sala.");
 
+let continuarOCancelar = prompt("Si desea ver nuestros próximos estrenos en cartelera escriba Aceptar, de lo contrario, escriba Cancelar para finalizar con su solicitud");
+
+
+while((continuarOCancelar != "Aceptar") && (continuarOCancelar !== "Cancelar")){
+    alert("Marque una opción correcta");
+    continuarOCancelar = prompt("Seleccione la opción correcta");
+}
+
+if(continuarOCancelar == "Aceptar"){
+    alert("Estrenos: Jurassic Park, Anabelle, El Conjuro, Matrix 2, Rapido y Furioso 6, Mike Tyzon: La Historia, Tu amigo fiel.")
+}
+
+class Estrenos {
+    constructor (nombre, fechaestreno) {
+
+    this.nombre = nombre;
+    this.fechaestreno = fechaestreno;
+}
+}
+const carteleraYEstrenos = [];
+carteleraYEstrenos.push(new Estrenos("Jurassic Park", "10 DIC"));
+carteleraYEstrenos.push(new Estrenos("Anabelle", "12 DIC"));
+carteleraYEstrenos.push(new Estrenos("El Conjuro", "13 DIC"));
+carteleraYEstrenos.push(new Estrenos("Matrix 2", "14 DIC"));
+carteleraYEstrenos.push(new Estrenos("Rapido y Furioso 6", "15 DIC"));
+carteleraYEstrenos.push(new Estrenos("Mike Tyzon: La Historia", "16 DIC"));
+carteleraYEstrenos.push(new Estrenos("Tu amigo fiel", "17 DIC"));
+
+for(const estrenos of carteleraYEstrenos)
+console.log(carteleraYEstrenos);
+
+const estrenosDiciembre = carteleraYEstrenos.find((el) => el.nombre === "Jurassic Park");
+const estrenosDiciembre1 = carteleraYEstrenos.find((el) => el.nombre === "Anabelle");
+const estrenosDiciembre2 = carteleraYEstrenos.find((el) => el.nombre === "Tu amigo fiel");
+console.log(estrenosDiciembre)
+console.log(estrenosDiciembre1)
+console.log(estrenosDiciembre2)
+
+
+
 
 function agradecimientoEmail () {
     
     alert("Gracias por elegir este horario, a continuación indica tu e-mail para que te enviemos la ubicación en sala.");
     
 }
+
+
